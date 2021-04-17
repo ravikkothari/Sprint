c8d3c976605a31f34824441fe95f572f85bef763
Parth <Parth@LAPTOP-2MAOKFPO>
2021-04-18 01:23:53 +0530

    Changed Validation

--- CustomerPlay/src/main/java/com/cg/entities/Customer.java
+++ CustomerPlay/src/main/java/com/cg/entities/Customer.java
@@ -3,93 +3,143 @@ package com.cg.entities;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="Customer")
+@Table(name="managecustomer")
 public class Customer {
-	 @Id
-	 @Column(name="cust_Id")
-	 private int id;
-	 private String name;
-	 private int age;
-	 private String password;
-	 
-	 @OneToMany(targetEntity = CustomerPlaycard.class, cascade = CascadeType.ALL)
-	 @JoinColumn(name = "cp_fk", referencedColumnName = "cust_Id")
-	 private List<CustomerPlaycard> CustomerPlaycard;
 	
 
-	public Customer() {
-		super();
-
-	}
+	@Id
+	private int customerId;
+	
+	
+	@NotEmpty(message = "Please enter a user name")
+	@Size(min = 1, max = 20, message = "Name must be between 1 and 20 characters")
+	private String  customerName;
+	
+	@NotEmpty(message = "Please enter the password")
+	@Size(min = 4, max = 20, message = "Password must be between 4 and 8 characters")
+	private String  customerPassword;
+	
+	
+	@NotEmpty(message = "Please enter the email")
+	@Email(message = "Email should be valid")
+	private String  customerEmail;
 
 	
 
-	public Customer(int id, String name, int age, String password,
-			List<com.cg.entities.CustomerPlaycard> customerPlaycard) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-		this.password = password;
-		CustomerPlaycard = customerPlaycard;
-	}
+	
+	
+	@OneToMany(targetEntity = PlayCard.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "ap_fk", referencedColumnName = "customerId")
+	private List<PlayCard> playcards;
+		
+		public Customer() {
+			super();
+
+		}
+	
+	
+		
+		public Customer(int customerId,
+				@NotEmpty(message = "Please enter a user name") @Size(min = 1, max = 20, message = "Name must be between 1 and 20 characters") String customerName,
+				@NotEmpty(message = "Please enter the password") @Size(min = 4, max = 20, message = "Password must be between 4 and 8 characters") String customerPassword,
+				@NotEmpty(message = "Please enter the email") @Email(message = "Email should be valid") String customerEmail) {
+			super();
+			this.customerId = customerId;
+			this.customerName = customerName;
+			this.customerPassword = customerPassword;
+			this.customerEmail =  customerEmail;
+		}
 
 
 
-	public int getId() {
-		return id;
-	}
+		public int getCustomerId() {
+			return customerId;
+		}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+		public void setCustomerId(int customerId) {
+			this.customerId = customerId;
+		}
 
-	public int getAge() {
-		return age;
-	}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
 
-	public List<CustomerPlaycard> getCustomerPlaycard() {
-		return CustomerPlaycard;
-	}
+		public String getCustomerName() {
+			return customerName;
+		}
 
-	public void setCustomerPlaycard(List<CustomerPlaycard> customerPlaycard) {
-		CustomerPlaycard = customerPlaycard;
-	}
 
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+		public void setCustomerName(String customerName) {
+			this.customerName = customerName;
+		}
 
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", age=" + age + ", password=" + password
-				+ ", CustomerPlaycard=" + CustomerPlaycard + "]";
-	}
 
+
+		public String getCustomerPassword() {
+			return customerPassword;
+		}
+
+
+
+		public void setCustomerPassword(String customerPassword) {
+			this.customerPassword = customerPassword;
+		}
+
+
+
+		public String getCustomerEmail() {
+			return customerEmail;
+		}
+
+
+
+		public void setCustomerEmail(String customerEmail) {
+			this.customerEmail = customerEmail;
+		}
+
+
+
+		public List<PlayCard> getPlaycards() {
+			return playcards;
+		}
+
+
+
+		public void setPlaycards(List<PlayCard> playcards) {
+			this.playcards = playcards;
+		}
+
+
+
+		@Override
+		public String toString() {
+			return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", customerPassword="
+					+ customerPassword + ", customerEmail=" + customerEmail + ", playcards=" + playcards + "]";
+		}
+
+
+		
+
+		
+		
+	
+			
+		
+			
+
+	
 	
 	
 }
+
